7f16ab9b597241efbf3a393841060facdb57a185
Loi Tran <Loi Tran>
2017-03-01 20:02:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/windows10timt/helloworld/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.windows10timt.helloworld;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        NhanVien Loi = new NhanVien(1996, "Tran Minh Loi");
+        String s = Loi.Hoten + Loi.namsinh.toString() + Loi.tuoi.toString();
+        System.out.println(s);
+        switch (tuoi)
+        {case 1  tuoi++;
+        break;
+        case 2 }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
---
+++ app/src/main/java/com/example/windows10timt/helloworld/NhanVien.java
@@ -0,0 +1,19 @@
+package com.example.windows10timt.helloworld;
+
+/**
+ * Created by Windows 10 TIMT on 28/2/2017.
+ */
+
+public class NhanVien {
+    public String Hoten;
+    public Integer namsinh;
+    public Integer tuoi;
+
+
+    public NhanVien(Integer namsinh, String hoten) {
+        this.namsinh = namsinh;
+        Hoten = hoten;
+        tuoi=2017-namsinh;
+
+    }
+}
